5e1978420685f8ea16bf001cc2eada0e257a2be3
Pratibha <157950484+Pratibha0801@users.noreply.github.com>
2024-07-18 17:32:47 +0530

    added day1.js

---
+++ day1.js
@@ -0,0 +1,43 @@
+// activity1
+//task1
+var x=11;
+console.log("var num: ",x);  //output == var num: 11
+
+//task 2
+let str="hello";
+console.log("string: ",str);  //output == string: hello
+
+//activity 2
+//task 3
+const bool = true;
+console.log("boolean: ",bool);  //output == boolean: true
+
+//activity 3
+//task4
+let num = 10; //number
+let string = "hello"; //string
+let b = false; //boolean
+let obj = {  //object
+    name: 'Pratibha',
+    branch: 'ECE'
+}
+let arr=[1,2,3,4]; //array
+console.log(typeof(num)); //output: number
+console.log(typeof(string)); //output: string
+console.log(typeof(b)); //output: boolean
+console.log(typeof(obj)); //output: object
+console.log(typeof(2)); //number
+
+//activity 4
+//task5:
+let num2 = 1;
+console.log("initial value: ",num2);  //output = initial value: 1
+num2= 2;
+console.log("initial value: ",num2); //output = initial value: 2
+
+//activity 5
+//task6:
+const str2 = "hello";
+console.log(str2); //output: hello
+// str2 = "hey";
+// console.log(str2); TypeError: Assignment to constant variable.
